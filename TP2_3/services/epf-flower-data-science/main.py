0360198f81d742c9153a7f43e6fb48646aef426a
malol <malo.lajous@epfedu.fr>
2024-11-26 15:08:45 +0100

    redirect root API to "/docs"

--- TP2_3/services/epf-flower-data-science/main.py
+++ TP2_3/services/epf-flower-data-science/main.py
@@ -1,8 +1,15 @@
 import uvicorn
-
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from src.app import get_application
 
 app = get_application()
+
+#redirect root API to automatic swagger documentation
+@app.get("/")
+async def redirect_root():
+    return RedirectResponse(url="/docs")
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True, port=8080)
